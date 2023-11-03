0efd5d31c1dcba4ba8ac11c97c07f7b709f10b61
peilutu <85421108+peilutu@users.noreply.github.com>
2023-11-02 17:34:38 -0700

    add models and routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workoutModel");
-
 //GET all workouts
 router.get("/", (req, res) => {
   res.json({ mssg: "GET all workouts" });
@@ -15,8 +14,14 @@ router.get("/:id", (req, res) => {
 //POST a new workout
 router.post("/", async (req, res) => {
   const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 
-  res.json({ mssg: "POST a new workout" });
+  //   res.json({ mssg: "POST a new workout" });
 });
 
 //DELETE a workout
